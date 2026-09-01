19f545be6ff265fe935dc6390b6a8800862fedbd
Space653000 <137800223+Space653000@users.noreply.github.com>
2026-09-01 10:20:59 +0800

    test: reject archive links and special restore members

--- tests/test_private_state.py
+++ tests/test_private_state.py
@@ -1,4 +1,5 @@
 import importlib.util
+import tarfile
 import unittest
 from pathlib import Path
 
@@ -19,6 +20,26 @@ class PrivateStateTests(unittest.TestCase):
 
     def test_absolute_path_is_denied(self):
         self.assertFalse(module.safe_member("/etc/passwd"))
+
+    def test_windows_drive_path_is_denied(self):
+        self.assertFalse(module.safe_member("C:\\Windows\\System32\\evil.txt"))
+
+    def test_symlink_member_is_denied(self):
+        member = tarfile.TarInfo("data/link")
+        member.type = tarfile.SYMTYPE
+        member.linkname = "../../secret"
+        self.assertFalse(module.safe_member(member))
+
+    def test_hardlink_member_is_denied(self):
+        member = tarfile.TarInfo("data/hardlink")
+        member.type = tarfile.LNKTYPE
+        member.linkname = "data/other"
+        self.assertFalse(module.safe_member(member))
+
+    def test_fifo_member_is_denied(self):
+        member = tarfile.TarInfo("data/fifo")
+        member.type = tarfile.FIFOTYPE
+        self.assertFalse(module.safe_member(member))
 
 
 if __name__ == "__main__":
